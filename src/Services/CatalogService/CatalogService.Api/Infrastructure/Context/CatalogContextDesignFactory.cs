9e865ee06f6d96027b61f6024f6bde083c4d86b7
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-11-25 16:23:26 +0300

    Catalog serrvice implementation

---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContextDesignFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Api.Infrastructure.Context
+{
+    public class CatalogContextDesignFactory : IDesignTimeDbContextFactory<CatalogContext>
+    {
+        public CatalogContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>()
+                .UseSqlServer("Data Source=localhost;Initial Catalog=catalog;Persist Security Info=True;User ID=sa;Password=Salih123!");
+
+            return new CatalogContext(optionsBuilder.Options);
+        }
+    }
+}
--- src/Services/CatalogService/CatalogService.Api/Startup.cs
+++ src/Services/CatalogService/CatalogService.Api/Startup.cs
@@ -1,15 +1,11 @@
+using CatalogService.Api.Extensions;
+using CatalogService.Api.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace CatalogService.Api
 {
@@ -27,6 +23,13 @@ namespace CatalogService.Api
         {
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogService.Api", Version = "v1" });
+            });
+
+            services.Configure<CatalogSettings>(Configuration.GetSection("CatalogSettings"));
+            services.ConfigureDbContext(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,8 @@ namespace CatalogService.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogService.Api v1"));
             }
 
             app.UseHttpsRedirection();

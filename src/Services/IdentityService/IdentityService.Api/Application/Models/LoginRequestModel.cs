bbc790e9cde358dfaa12761ec280ded6ef72db3f
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-11-25 23:03:22 +0300

    Identity service with Swagger implemented.

---
+++ src/Services/IdentityService/IdentityService.Api/Application/Models/LoginRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityService.Api.Application.Models
+{
+    public class LoginRequestModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
--- src/Services/IdentityService/IdentityService.Api/Startup.cs
+++ src/Services/IdentityService/IdentityService.Api/Startup.cs
@@ -1,15 +1,10 @@
+using IdentityService.Api.Application.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace IdentityService.Api
 {
@@ -25,8 +20,14 @@ namespace IdentityService.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IIdentityService, IdentityService.Api.Application.Services.IdentityService>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentService.Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,8 @@ namespace IdentityService.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService.Api v1"));
             }
 
             app.UseHttpsRedirection();

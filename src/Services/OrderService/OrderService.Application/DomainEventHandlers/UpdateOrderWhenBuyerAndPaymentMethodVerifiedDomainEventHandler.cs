4e827eccf2e8d3ddeab2084adc31aa36af3bc0d8
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-12-02 14:53:10 +0300

    Order Application  layer implementation.

---
+++ src/Services/OrderService/OrderService.Application/DomainEventHandlers/UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using OrderService.Application.Interfaces.Repositories;
+using OrderService.Domain.Events;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.DomainEventHandlers
+{
+    public class UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler : INotificationHandler<BuyerAndPaymentMethodVerifiedDomainEvent>
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler(
+            IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        }
+
+        public async Task Handle(BuyerAndPaymentMethodVerifiedDomainEvent buyerPaymentMethodVerifiedEvent, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await orderRepository.GetByIdAsync(buyerPaymentMethodVerifiedEvent.OrderId);
+            orderToUpdate.SetBuyerId(buyerPaymentMethodVerifiedEvent.Buyer.Id);
+            orderToUpdate.SetPaymentMethodId(buyerPaymentMethodVerifiedEvent.Payment.Id);
+
+            // set methods so validate
+        }
+    }
+}
---
+++ src/Services/OrderService/OrderService.Application/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace OrderService.Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services, Type startup)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+
+            services.AddAutoMapper(assm);
+            services.AddMediatR(assm);
+
+            return services;
+        }
+    }
+}
